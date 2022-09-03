c2e9e8040714d4c881755f2e53684e2d20c76100
KevinBae00 <109911321+KevinBae00@users.noreply.github.com>
2022-09-04 02:51:31 +0900

    Project1

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,34 +1,51 @@
 package org.example;
 
-import java.lang.reflect.Array;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
     ArrayList<Word> list;
-    Scanner s;
+    BufferedReader br;
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    WordCRUD(Scanner s){
+    WordCRUD(BufferedReader br){
         list = new ArrayList<>();
-        this.s = s;
+        this.br = br;
     }
 
     @Override
     public Object add() {
-        System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
-        int level = s.nextInt();
-        String word = s.nextLine();
+        try {
+            bw.write("=> 난이도(1,2,3) & 새 단어 입력 : ");
+            bw.flush();
+            int level = br.read()-48;
+            String word = br.readLine();
 
-        System.out.print("뜻 입력 : ");
-        String meaning = s.nextLine();
+            bw.write("뜻 입력 : ");
+            bw.flush();
+            String meaning = br.readLine();
+            return new Word(0, level, word, meaning);//Object로 리턴
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        return new Word(0,level,word,meaning);//Object로 리턴
+
     }
 
     public void addWord(){
         Word one = (Word) add();//(Word type)add함수 실행
         list.add(one);
-        System.out.println("새 단어가 단어장에 추가되었습니다. ");
+        try {
+            bw.write("새 단어가 단어장에 추가되었습니다. ");
+            bw.newLine();
+            bw.flush();
+        }catch (IOException e){
+            throw new RuntimeException("단어 출력 오류");
+        }
     }
 
     @Override
@@ -46,11 +63,23 @@ public class WordCRUD implements ICRUD{
 
     }
     public void listAll(){
-        System.out.println("-----------------------------------");
-        for(int i = 0; i < list.size(); i++){
-            System.out.print((i+1) + " ");
-            System.out.println(list.get(i).toString());
+        try {
+            bw.write("-----------------------------------");
+            bw.newLine();
+            bw.flush();
+            for(int i = 0; i < list.size(); i++){
+                bw.write((i+1) + " ");
+                bw.flush();
+                bw.write(list.get(i).toString());
+                bw.newLine();
+                bw.flush();
+            }
+            bw.write("-----------------------------------");
+            bw.newLine();
+            bw.flush();
+        }catch (IOException e){
+            throw new RuntimeException("listAll() 파일에서 오류 발생");
         }
-        System.out.println("-----------------------------------");
+
     }
 }
--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -1,35 +1,57 @@
 package org.example;
 
-import java.util.Scanner;
+//import java.util.Scanner;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class WordManager {
 
-    Scanner s = new Scanner(System.in);
+  //  Scanner s = new Scanner(System.in);
+  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     WordCRUD wordCRUD;
 
     WordManager(){
-        wordCRUD = new WordCRUD(s);
+        wordCRUD = new WordCRUD(br);
     }
     public int selectMenu(){
-        System.out.print("*** 영단어 마스터 ***\n"
-                + "********************\n"
-                + "1. 모든 단어 보기\n"
-                + "2. 수준별 단어 보기\n"
-                + "3. 단어 검색\n"
-                + "4. 단어 추가\n"
-                + "5. 단어 수정\n"
-                + "6. 단어 삭제\n"
-                + "7. 파일 저장\n"
-                + "0. 나가기\n"
-                + "********************\n"
-                + "=> 원하는 메뉴는? ");
-        return s.nextInt();
+        try {
+            bw.write("""
+                    *** 영단어 마스터 ***
+                    ********************
+                    1. 모든 단어 보기
+                    2. 수준별 단어 보기
+                    3. 단어 검색
+                    4. 단어 추가
+                    5. 단어 수정
+                    6. 단어 삭제
+                    7. 파일 저장
+                    0. 나가기
+                    ********************
+                    => 원하는 메뉴는?\s""");
+            bw.flush();
+            return Integer.parseInt(br.readLine());
+        }catch (IOException e){
+            throw new RuntimeException("숫자만 가능합니다!");
+        }
+
     }
 
     public void start(){
         while (true) {
             int menu = selectMenu();
-            if(menu == 0) break;//0을 입력 받으면 실행종료
+            if(menu == 0) {
+                try {
+                    bw.close();
+                    break;//0을 입력 받으면 실행종료
+                }catch (IOException e){
+                    throw new RuntimeException("flush,close 에러");
+                }
+            }
             if(menu == 4){//4를 입력 받으면 단어 추가
                 wordCRUD.addWord();
             }
